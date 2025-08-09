540315338df594e0f8fe1d851c2990f967dfbfa5
CodeSShuv <codeshuv@gmail.com>
2025-08-09 12:30:51 +0545

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./App.css";
 import Home from "./pages/Home.jsx";
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
+
 const App = () => {
+  const [scrollPosition,setScrollPosition] = useState(0);
+  
   return (
     <>
-    <Navbar/>
-    <Home/>
+    <Navbar scrollPosition = {scrollPosition}/>
+    <Home />
     <Footer/>
     </>
   )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,10 +35,28 @@ const Footer = () => {
         </div>
       </div>
       <div className="mid">
+        <div className='logo-container'>
+
         <img src="./map.png" alt="" />
+        </div>
+        <div className="follow-credential">
+          <div className='attributes'>Follow Us</div>
+
+          <div className='follow-links'>
+            <span>
+              <img src="/facebook.png" alt="" />
+            </span>
+            <span>
+              <img src="/instagram.png" alt="" />
+            </span>
+            <span>
+              <img src="/trivago.png" alt="" />
+            </span>
+          </div>
+        </div>
       </div>
       <div className="right">
-        
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2517.5427961790597!2d4.698015011609142!3d50.87666347155859!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c160d872f3b16d%3A0x620185fef3a5c3eb!2sNew%20Holy%20Cow!5e0!3m2!1sen!2snp!4v1754713313352!5m2!1sen!2snp" width="400" height="300" style={{border:"0"}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe> 
       </div>
     </div>
   )
